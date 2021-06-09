b8caf24b9157f6b980949fb09c8c8515ef517e53
Anatolii <anatolii@rhythmic-rebellion.com>
2021-06-09 11:29:25 +0300

    implemented chat screen

--- chat/lib/screens/chat_screen.dart
+++ chat/lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chat/widgets/chat/messages.dart';
+import 'package:chat/widgets/chat/new_message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -38,30 +40,13 @@ class ChatScreen extends StatelessWidget {
           )
         ],
       ),
-      body: StreamBuilder(
-          stream: Firestore.instance
-              .collection('chats/FH629IKuXsrhnKCMwckQ/messages')
-              .snapshots(),
-          builder: (ctx, streamSnapshot) {
-            if (streamSnapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            }
-            final documents = streamSnapshot.data.documents;
-            return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(8),
-                child: Text(documents[index]['text']),
-              ),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/FH629IKuXsrhnKCMwckQ/messages')
-              .add({'text': 'Button clicked!'});
-        },
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }
---
+++ chat/lib/widgets/chat/messages.dart
@@ -0,0 +1,42 @@
+import 'package:chat/widgets/chat/message_bubble.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, currentUserSnapshot) {
+        if (currentUserSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return StreamBuilder(
+          stream: Firestore.instance
+              .collection('chat')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (ctx, chatSnapshot) {
+            if (chatSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              final chatDocs = chatSnapshot.data.documents;
+              return ListView.builder(
+                reverse: true,
+                itemCount: chatDocs.length,
+                itemBuilder: (ctx, index) => MessageBubble(
+                  ValueKey(chatDocs[index].documentID),
+                  chatDocs[index]['text'],
+                  chatDocs[index]['userId'] == currentUserSnapshot.data.uid,
+                  chatDocs[index]['username'],
+                ),
+              );
+            }
+          },
+        );
+      },
+    );
+  }
+}
